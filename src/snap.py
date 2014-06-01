d535fe0f9217ec8faacb8988941a5226787988d8
Admin <hue@humairahmed.com>
2014-05-31 18:08:42 -0700

    Added FTP functionality and cleaned-up code

--- src/snap.py
+++ src/snap.py
@@ -1,6 +1,6 @@
 #/****************************************************************/ 
 # Program:  DierectorySnapshot
-# Version:  1.0
+# Version:  2.0
 # Date:     05/31/2014
 # Website:  http://www.HumairAhmed.com
 #
@@ -22,10 +22,22 @@
 #    1. Create directory snapshot
 #    2, List directory snapshot files
 #    3. Compare directory snapshot files
-#    4. Help
-#    5. Exit
+#    4. Settings
+#    5. Help
+#    6. Exit
 #    --------------------------------------------------
 #
+# Selecting #4 above leads you to another submenu where you can setup FTP, so, upon 
+# creation, '.snap' files are uploaded to a FTP server. Below is the submenu.
+#
+#   --------------------------------------------------
+#    Please select a number and press enter:
+#    
+#    1. Change local folder to automatically save '.snap' files to
+#    2. Setup FTP to automatically upload '.snap' files to FTP server
+#    3. Disable FTP upload (will delete all saved FTP information)
+#    4. Return to main menu
+#    --------------------------------------------------
 #  
 # License:
 # 
@@ -37,6 +49,7 @@
 
 import os, snaphelper
 
+global programDir
 
 #menu function
 def createMenu():
@@ -48,8 +61,26 @@ def createMenu():
     1. Create directory snapshot
     2, List directory snapshot files
     3. Compare directory snapshot files
-    4. Help
-    5. Exit
+    4. Settings
+    5. Help
+    6. Exit
+    --------------------------------------------------''')
+    
+    action = input("\t")
+    
+    return action
+
+
+def createSubmenu():
+    os.system('clear')
+    print('''
+    --------------------------------------------------
+    Please select a number and press enter:
+    
+    1. Change local folder to automatically save '.snap' files to
+    2. Setup FTP to automatically upload '.snap' files to FTP server
+    3. Disable FTP upload (will delete all saved FTP information)
+    4. Return to main menu
     --------------------------------------------------''')
     
     action = input("\t")
@@ -58,34 +89,60 @@ def createMenu():
 
 
 def main():
+    programDir = os.getcwd()
+    
+    
     menu_selection = ""
     
-    while menu_selection != "5":
+    while menu_selection != "6":
         menu_selection = createMenu()
         
-        if menu_selection == "1":
+        if (menu_selection == "1"):
             os.system('clear')
             
             snap_directory = input("Enter directory path of the directory to take a snapshot of: ")
             snap_filename =  input("Enter name of directory snapshot file to create: ")
             
-            snaphelper.createSnap(snap_directory, snap_filename + ".snap")
+            snaphelper.createSnap(snap_directory, snap_filename)
             
-        elif menu_selection == "2":
+        elif (menu_selection == "2"):
             os.system('clear')
             snaphelper.listSnaps()
             
-        elif menu_selection == "3":
+        elif (menu_selection == "3"):
             os.system('clear')
             file1 = input("Enter the filename of snapshot 1: ")
             file2 = input("Enter the filename of snapshot 2: ")
             
             snaphelper.compareSnaps(file1, file2)
             
-        elif menu_selection == "4":
+        elif (menu_selection == "4"):
+            submenu_selection = ""
+            
+            while (submenu_selection != "4"):
+                submenu_selection = createSubmenu()
+                
+                if (submenu_selection == "1"):
+                    print("This feature has not yet been implemented!")
+                    input("\nPress [Enter] to continue...")
+                    
+                elif (submenu_selection == "2"):
+                    os.system('clear')
+                    snaphelper.setupFTP(programDir)
+                    
+                elif (submenu_selection == "3"):
+                    os.system('clear')
+                    snaphelper.disableFTP(programDir)
+                    
+                else:
+                    if menu_selection != "4":
+                        snaphelper.invalidMenuOperation()
+            
+        elif menu_selection == "5":
             snaphelper.helpMe()
+            
         else:
-            if menu_selection != "5":
+            if menu_selection != "6":
                 snaphelper.invalidMenuOperation()
             
     
--- src/snaphelper.py
+++ src/snaphelper.py
@@ -1,6 +1,6 @@
 #/****************************************************************/ 
 # Program:  DierectorySnapshot
-# Version:  1.0
+# Version:  2.0
 # Date:     05/31/2014
 # Website:  http://www.HumairAhmed.com
 #
@@ -22,10 +22,22 @@
 #    1. Create directory snapshot
 #    2, List directory snapshot files
 #    3. Compare directory snapshot files
-#    4. Help
-#    5. Exit
+#    4. Settings
+#    5. Help
+#    6. Exit
 #    --------------------------------------------------
 #
+# Selecting #4 above leads you to another submenu where you can setup FTP, so, upon 
+# creation, '.snap' files are uploaded to a FTP server. Below is the submenu.
+#
+#   --------------------------------------------------
+#    Please select a number and press enter:
+#    
+#    1. Change local folder to automatically save '.snap' files to
+#    2. Setup FTP to automatically upload '.snap' files to FTP server
+#    3. Disable FTP upload (will delete all saved FTP information)
+#    4. Return to main menu
+#    --------------------------------------------------
 #  
 # License:
 # 
@@ -35,9 +47,17 @@
 # Can edit and redistribute code as long as above reference of authorship is kept within the code.
 #/****************************************************************/
 
-import os, sys, pickle, difflib
+import os, sys, pickle, difflib, ftplib
+
+global snap_ext
+global ftp_file 
+global enter_to_continue
 
 
+snap_ext = ".snap"
+ftp_file = "ftp.dat"
+enter_to_continue = "Press [Enter] to continue..."
+    
 
 def printList(snap_list):
     index = 1
@@ -56,8 +76,8 @@ def printList(snap_list):
 
 
 def invalidMenuOperation():
-    sys.stderr.write("INVLALID MENU CHOICE; PLEASE SELECT A VALID OPERATION!")
-    input("Press [Enter] to continue...")
+    sys.stderr.write("INVLALID MENU CHOICE; PLEASE SELECT A VALID OPERATION!\n")
+    input(enter_to_continue)
     return
 
 
@@ -77,7 +97,7 @@ def helpMe():
     5. Exit
     ''')
     
-    input("Press [Enter] to continue...")
+    input(enter_to_continue)
     return
 
 
@@ -110,8 +130,49 @@ def changeToSnapshotDir():
         return
 
 
-def compareSnaps(filename1, filename2):
+def setupFTP(ftp_file_dir):
+    overwrite = ""
     
+    os.chdir(ftp_file_dir )
+    
+    ftp_data_exists = os.path.exists(ftp_file)
+    
+    if(ftp_data_exists):
+        while(overwrite != 'y' and overwrite != 'n'):
+            overwrite = input("FTP information already exists! Continue and overwrite? (y/n)")
+        
+    if(overwrite == 'y' or ftp_data_exists == False):  
+        ftp_url = input("Enter FTP URL or IP: ")
+        ftp_username = input("Enter FTP username: ")
+        ftp_password = input("Enter FTP password: ")
+        
+        ftp_credentials = [ftp_url, ftp_username, ftp_password]
+        settingsFile = open(ftp_file, 'wb')
+        pickle.dump(ftp_credentials, settingsFile, -1)
+        
+        print("\nFTP information successfully saved and will be used for storing directory snapshots.")
+        input(enter_to_continue)
+    else:
+        print("\nNo changes made")
+        input(enter_to_continue)
+    
+    
+def disableFTP(ftp_file_dir):
+    os.chdir(ftp_file_dir)
+                    
+    if(os.path.exists(ftp_file)):
+        os.remove(ftp_file)
+    
+        print("FTP information successfully deleted and FTP upload disabled!")
+        input(enter_to_continue)
+                        
+    else:
+        print("Error: FTP was never setup/enabled!")
+        input(enter_to_continue)
+    
+
+
+def compareSnaps(filename1, filename2):
     filename1 = filename1 + ".snap"
     filename2 = filename2 + ".snap"
         
@@ -135,7 +196,7 @@ def compareSnaps(filename1, filename2):
         
     except:
         print("Errors encountered on accessing directory snapshot files!")
-        input("Press [Enter] to continue...")
+        input(enter_to_continue)
         return
     
     dirs_diff = list(difflib.unified_diff(dirs1, dirs2))
@@ -171,7 +232,7 @@ def compareSnaps(filename1, filename2):
     print("\n\nRemoved Files:\n")
     printList(removed_files)
     
-    input("Press [Enter] to continue...")
+    input(enter_to_continue)
     return
     
 
@@ -189,16 +250,33 @@ def createSnap(directory, filename):
             
         changeToSnapshotDir()
         
+        filename = filename + snap_ext
+        
         snapFile = open(filename, 'wb')
         pickle.dump(all_dirs, snapFile, -1)
         pickle.dump(all_files, snapFile, -1)
         snapFile.close()
         
+        os.chdir("..")
+        if (os.path.exists(ftp_file)):
+            ftp_file_handle = open(ftp_file, 'rb')
+            ftp_credentials = pickle.load(ftp_file_handle)
+            ftp_file_handle.close()
+            
+            changeToSnapshotDir()
+            snapFile = open(filename, 'rb')
+            
+            ftp = ftplib.FTP(ftp_credentials[0], ftp_credentials[1], ftp_credentials[2])
+            
+            ftp.storbinary("STOR " + filename, snapFile)
+            ftp.close()
+            snapFile.close()
+            
     except:
         print("Errors on attempting to save directory snapshot file!")
         
     
-    input("Press [Enter] to continue...")
+    input(enter_to_continue)
     return
 
 
@@ -210,7 +288,7 @@ def listSnaps():
         file_list = os.listdir(os.curdir)
     
         for item in file_list:
-            if item.find(".snap") != -1:
+            if item.find(snap_ext) != -1:
                 snap_list.append(item)
             
         print('''
@@ -222,10 +300,10 @@ def listSnaps():
     
     except:
         print("Errors encountered on accessing directory snapshot files!")
-        input("Press [Enter] to continue...")
+        input(enter_to_continue)
         return
 
-    input("\nPress [Enter] to continue...")
+    input("\n" + enter_to_continue)
     return
 
 
